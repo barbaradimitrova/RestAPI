4cf806183b826f93e65e58ecc25ca3c998cb95a0
barbaradimitrova <barbara.dimitrova@goeuro.com>
2018-01-02 16:56:42 +0100

    moved config arguments to main body

--- app.py
+++ app.py
@@ -18,9 +18,19 @@ from werkzeug.utils import redirect
 app = Flask(__name__)
 mail = Mail(app)
 app.config['SECRET_KEY'] = 'once upon a time'
+app.config['SECURITY_PASSWORD_SALT'] = 'my_precious_two'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['N_TOKENS'] = 1
+app.config['MAIL_SERVER'] = os.environ.get('APP_MAIL_SERVER', 'smtp.googlemail.com')
+app.config['MAIL_PORT'] = int(os.environ.get('APP_MAIL_PORT', 465))
+
+    # mail authentication
+app.config['MAIL_USERNAME'] = os.environ.get('APP_MAIL_USERNAME', None)
+app.config['MAIL_PASSWORD'] = os.environ.get('APP_MAIL_PASSWORD', None)
+
+    # mail accounts
+app.config['MAIL_DEFAULT_SENDER'] = 'from@example.com'
 # extensions
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
@@ -30,16 +40,16 @@ auth = HTTPBasicAuth()
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(32), index=True)
+    username = db.Column(db.String(32), index=True)
     password_hash = db.Column(db.String(64))
     registered_on = db.Column(db.DateTime, nullable=False)
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)
     password_reset_token = db.Column(db.String, nullable=True)
 
-    def __init__(self, email, password, confirmed,
+    def __init__(self, username, password, confirmed,
                  confirmed_on=None):
-        self.email = email
+        self.username = username
         self.password = pwd_context.encrypt(password)
         self.registered_on = datetime.datetime.now()
         self.confirmed = confirmed
@@ -48,38 +58,44 @@ class User(db.Model):
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
 
-    def verify_email(email):
-        return validate_email(email,check_mx=True)
+def verify_email(email):
+    return validate_email(email,check_mx=True)
 
 
-#user log in
+# user log in
 @app.route('/api/users/login', methods=['POST'])
 def verify_password(email, password):
     user = User.query.filter_by(email=email).first()
     if not user or not user.verify_password(password):
-        abort(400) #can not log in
+        abort(400)      # can not log in
         return jsonify({'User email or password incorrect. Access Denied.'})
     g.user = user
     return True
 
-#user sign up
+
+# user sign up
 @app.route('/api/users/signup', methods=['POST'])
 def new_user():
-    email = request.json.get('email')
+    email = request.json.get('username')
     password = request.json.get('password')
     if email is None or password is None:
-        abort(400)    # missing arguments
-    if User.query.filter_by(email=email).first() is not None:
-        abort(400)    # existing user
-    if not email.verify_email(email):
-        abort(400)   # not valid email
+        flash('Please provide email and password')
+       # abort(400)    # missing arguments
+
+    if User.query.filter_by(username=email).first() is not None:
+        flash('User exists already',"danger")
+        #abort(400)    # existing user
+    if not verify_email(email):
+        #abort(400)   # not valid email
+        return jsonify({'Invalid email'})
     user = User(email, password, confirmed=False)
     db.session.add(user)
     db.session.commit()
     send_confirmation(email)
     return jsonify({'Please check you email to confirm registration.'})
 
-#getting a user from the DB
+
+# getting a user from the DB
 @app.route('/api/users/<int:id>')
 def get_user(id):
     user = User.query.get(id)
@@ -94,13 +110,16 @@ def get_auth_token():
     token = g.user.generate_auth_token(600)
     return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
-#welcome log in
+
+# welcome log in
 @app.route('/api/resource')
 @auth.login_required
 def get_resource():
+    flash('Welcome!', 'success')
     return jsonify({'data': 'Hello, %s!' % g.user.email})
 
-#email
+
+# email
 @app.route("/")
 def send_email(to, subject, template):
     msg = Message(
@@ -111,11 +130,12 @@ def send_email(to, subject, template):
     )
     mail.send(msg)
 
-#sendinf verification email
+
+# sending verification email
 @app.route('/send')
 def send_confirmation(email):
     token = generate_confirmation_token(email)
-    confirm_url = url_for('user.confirm_email', token=token, _external=True)
+    confirm_url = url_for('confirm_email', token=token, _external=True)
 
     html = "<p>Welcome! Thanks for signing up. Please follow this link to activate your account:</p>\
     <p><a href={}</a></p>\
@@ -127,12 +147,14 @@ def send_confirmation(email):
     flash('A confirmation email has been sent.', 'success')
     return redirect(url_for('user.unconfirmed'))
 
-#token generation for email confirmation
+
+# token generation for email confirmation
 def generate_confirmation_token(email):
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
 
-#confirming email token
+
+# confirming email token
 def confirm_token(token, expiration=3600):
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     try:
@@ -145,7 +167,8 @@ def confirm_token(token, expiration=3600):
         return False
     return email
 
-#path from confirmation email
+
+# path from confirmation email
 @app.route('/confirm/<token>')
 def confirm_email(token):
     if User.confirmed:
